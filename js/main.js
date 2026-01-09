4554fb14497cd79a21e8d522bdf37e39036c8100
Lamoscar30 <lamoscarachebe@gmail.com>
2026-01-09 20:40:37 +1200

    zulu time

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
-function scrollLeft() {
+/* function scrollLeft() {
   document.querySelector('.product-scroll').scrollBy({ left: -300, behavior: 'smooth' });
 }
 function scrollRight() {
   document.querySelector('.product-scroll').scrollBy({ left: 300, behavior: 'smooth' });
-}
+} */
+
+      const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
